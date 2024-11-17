298e580bfc0c5c410a3b1717891b97e7a80a49e6
milad <78633834+Milad-heydarloo@users.noreply.github.com>
2024-11-17 17:10:46 +0330

    yap
    njlja

--- lib/main.dart
+++ lib/main.dart
@@ -426,13 +426,13 @@
 //   }
 // }
 
-
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
 import 'package:wallet_finder/Getx_Wallet/Rout/AppRoutes.dart';
 
-
-void main() {
+Future<void> main() async {
+  await GetStorage.init();
   runApp(MyApp());
 }
 
