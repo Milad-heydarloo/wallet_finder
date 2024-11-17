d23b5c185a7123025880b43830c8be10ea658fe2
milad <78633834+Milad-heydarloo@users.noreply.github.com>
2024-11-17 17:05:25 +0330

    change shareprefrenss to get_storage

--- lib/Getx_Wallet/SharePrefrenss/share.dart
+++ lib/Getx_Wallet/SharePrefrenss/share.dart
@@ -1,11 +1,74 @@
-
-
-
-
-
-
-import 'package:get/get_utils/src/platform/platform.dart';
-import 'package:shared_preferences/shared_preferences.dart';
+//
+//
+//
+//
+//
+//
+// import 'package:get/get_utils/src/platform/platform.dart';
+// import 'package:shared_preferences/shared_preferences.dart';
+//
+// class SharedPreferencesHelper {
+//   static final SharedPreferencesHelper _instance =
+//   SharedPreferencesHelper._internal();
+//
+//   factory SharedPreferencesHelper() {
+//     return _instance;
+//   }
+//
+//   SharedPreferencesHelper._internal();
+//
+//   // بازیابی مقدار ذخیره شده برای botUrl
+//   Future<String?> getBotUrl() async {
+//     final prefs = await SharedPreferences.getInstance();
+//     return prefs.getString('botUrl');
+//   }
+//
+//   // بازیابی مقدار ذخیره شده برای termsAccepted
+//   Future<bool> isTermsAccepted() async {
+//     final prefs = await SharedPreferences.getInstance();
+//     return prefs.getBool('termsAccepted') ?? false;
+//   }
+//
+//   // بازیابی اطلاعات مربوط به remaining_days
+//   Future<String?> getRemainingDays() async {
+//     final prefs = await SharedPreferences.getInstance();
+//     return prefs.getString('expiration_time');
+//   }
+//
+//   // بازیابی اطلاعات مربوط به exists
+//   Future<bool> isLicenseValid() async {
+//     final prefs = await SharedPreferences.getInstance();
+//     return prefs.getBool('exists') ?? false;
+//   }
+//
+//   // // بازیابی اطلاعات مربوط به دستگاه برای Android
+//   // Future<String?> getAndroidDeviceId() async {
+//   //   final prefs = await SharedPreferences.getInstance();
+//   //   return prefs.getString('androidInfo');
+//   // }
+//   //
+//   // // بازیابی اطلاعات مربوط به دستگاه برای iOS
+//   // Future<String?> getIosDeviceId() async {
+//   //   final prefs = await SharedPreferences.getInstance();
+//   //   return prefs.getString('iosInfo');
+//   // }
+//
+//   Future<String?> getDeviceId() async {
+//     final prefs = await SharedPreferences.getInstance();
+//     String? deviceId;
+//
+//     if (GetPlatform.isAndroid) {
+//       deviceId = prefs.getString('androidInfo');
+//     } else if (GetPlatform.isIOS) {
+//       deviceId = prefs.getString('iosInfo');
+//     }
+//
+//     return deviceId;
+//   }
+//
+// }
+import 'package:get_storage/get_storage.dart';
+import 'package:get/get.dart';
 
 class SharedPreferencesHelper {
   static final SharedPreferencesHelper _instance =
@@ -19,51 +82,39 @@ class SharedPreferencesHelper {
 
   // بازیابی مقدار ذخیره شده برای botUrl
   Future<String?> getBotUrl() async {
-    final prefs = await SharedPreferences.getInstance();
-    return prefs.getString('botUrl');
+    final box = GetStorage();
+    return box.read('botUrl');
   }
 
   // بازیابی مقدار ذخیره شده برای termsAccepted
   Future<bool> isTermsAccepted() async {
-    final prefs = await SharedPreferences.getInstance();
-    return prefs.getBool('termsAccepted') ?? false;
+    final box = GetStorage();
+    return box.read('termsAccepted') ?? false;
   }
 
   // بازیابی اطلاعات مربوط به remaining_days
   Future<String?> getRemainingDays() async {
-    final prefs = await SharedPreferences.getInstance();
-    return prefs.getString('expiration_time');
+    final box = GetStorage();
+    return box.read('expiration_time');
   }
 
   // بازیابی اطلاعات مربوط به exists
   Future<bool> isLicenseValid() async {
-    final prefs = await SharedPreferences.getInstance();
-    return prefs.getBool('exists') ?? false;
+    final box = GetStorage();
+    return box.read('exists') ?? false;
   }
 
-  // // بازیابی اطلاعات مربوط به دستگاه برای Android
-  // Future<String?> getAndroidDeviceId() async {
-  //   final prefs = await SharedPreferences.getInstance();
-  //   return prefs.getString('androidInfo');
-  // }
-  //
-  // // بازیابی اطلاعات مربوط به دستگاه برای iOS
-  // Future<String?> getIosDeviceId() async {
-  //   final prefs = await SharedPreferences.getInstance();
-  //   return prefs.getString('iosInfo');
-  // }
-
+  // دریافت شناسه دستگاه
   Future<String?> getDeviceId() async {
-    final prefs = await SharedPreferences.getInstance();
+    final box = GetStorage();
     String? deviceId;
 
     if (GetPlatform.isAndroid) {
-      deviceId = prefs.getString('androidInfo');
+      deviceId = box.read('androidInfo');
     } else if (GetPlatform.isIOS) {
-      deviceId = prefs.getString('iosInfo');
+      deviceId = box.read('iosInfo');
     }
 
     return deviceId;
   }
-
 }
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,11 +6,11 @@ import FlutterMacOS
 import Foundation
 
 import device_info_plus
-import shared_preferences_foundation
+import path_provider_foundation
 import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
-  SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
